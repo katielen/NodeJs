6deecc93f00c4d9c1c78cbe526cf2984c7de09af
Katielen De Jesus <70919144+katielen@users.noreply.github.com>
2020-11-08 16:34:10 -0300

    Exercício heróis.

---
+++ PLAYGROUD/index.js
@@ -0,0 +1,13 @@
+const herois = [
+    {
+        nome: "batman",
+        superpoder: "inteligencia"
+    },
+    {
+        nome: "superman",
+        superpoder: "força"
+    }
+
+]
+module.exports = herois;
+// o module.exports serve para importar o nosso array até o nosso arquivo
